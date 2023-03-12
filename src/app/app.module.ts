8db72883c547dccb138af808fa22807dfcea3916
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2023-03-12 15:18:31 -0300

    Added angular animations package and set animations config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
-import { NgModule } from '@angular/core';
+import { NgModule, OnInit } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import { PrimeNGConfig } from 'primeng/api';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,9 +13,17 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule implements OnInit {
+
+  constructor(private primeNgConfig: PrimeNGConfig) { }
+
+  ngOnInit(): void {
+    this.primeNgConfig.ripple = true;
+  }
+}
