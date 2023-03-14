50640a8cd12a62e4d906cc736fa74ca772b3abee
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2023-03-14 20:07:47 -0300

    Added new updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,27 @@
 import { NgModule, OnInit } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { PrimeNGConfig } from 'primeng/api';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WeatherHomeComponent } from './modules/weather/components/weather-home/weather-home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherHomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    // PrimeNg
+    CardModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/modules/weather/weather.routing.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { WeatherHomeComponent } from './components/weather-home/weather-home.component';
-
-export const ROUTES: Routes = [
-    {
-        path: '',
-        component: WeatherHomeComponent
-    }
-]
