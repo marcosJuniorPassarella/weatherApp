50fc14326451162f18ee8a743feb022f85cb9359
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2023-03-18 10:38:55 -0300

    Implement weather service

--- src/app/modules/weather/services/weather.service.ts
+++ src/app/modules/weather/services/weather.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
+  private apiKey = '5089d009c55c68d90854027945fded44';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getWeatherDatas(cityName: string): Observable<any> {
+    return this.http
+      .get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${this.apiKey}`, {},)
+  }
 }
