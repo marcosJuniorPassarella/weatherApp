0124a45c97f7e97e4dfdea21aaa0e0a1f96859a0
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2023-03-18 11:20:46 -0300

    Implement new changes

--- src/app/modules/weather/components/weather-home/weather-home.component.ts
+++ src/app/modules/weather/components/weather-home/weather-home.component.ts
@@ -1,14 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { faTemperatureHigh, faTemperatureLow, faWind, faDroplet } from '@fortawesome/free-solid-svg-icons';
+import { WeatherService } from '../../services/weather.service';
+import { WeatherDatas } from 'src/app/models/weather.interface';
 
 @Component({
   selector: 'app-weather-home',
   templateUrl: './weather-home.component.html',
   styleUrls: []
 })
-export class WeatherHomeComponent {
+export class WeatherHomeComponent implements OnInit {
+  weatherDatas!: WeatherDatas;
+
   minTemperatureIcon = faTemperatureLow;
   maxTemperatureIcon = faTemperatureHigh;
   humidityIcon = faDroplet;
   windIcon = faWind;
+
+  constructor(private weatherService: WeatherService) { }
+
+  ngOnInit(): void {
+    this.weatherService.getWeatherDatas('Sidney')
+      .subscribe({
+        next: (response) => {
+          response && (this.weatherDatas = response);
+          console.log(this.weatherDatas);
+        }
+      })
+  }
 }
