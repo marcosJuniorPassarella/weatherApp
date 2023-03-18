5cda374ef81c173919cc559cc92719c3c0b29346
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2023-03-18 11:48:54 -0300

    Added onSubmit method

--- src/app/modules/weather/components/weather-home/weather-home.component.ts
+++ src/app/modules/weather/components/weather-home/weather-home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { faTemperatureHigh, faTemperatureLow, faWind, faDroplet } from '@fortawesome/free-solid-svg-icons';
+import { faTemperatureHigh, faTemperatureLow, faWind, faDroplet, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import { WeatherService } from '../../services/weather.service';
 import { WeatherDatas } from 'src/app/models/weather.interface';
 
@@ -10,21 +10,32 @@ import { WeatherDatas } from 'src/app/models/weather.interface';
 })
 export class WeatherHomeComponent implements OnInit {
   weatherDatas!: WeatherDatas;
+  cityName: string = 'São Paulo';
 
   minTemperatureIcon = faTemperatureLow;
   maxTemperatureIcon = faTemperatureHigh;
   humidityIcon = faDroplet;
   windIcon = faWind;
+  searchIcon = faMagnifyingGlass;
 
   constructor(private weatherService: WeatherService) { }
 
   ngOnInit(): void {
-    this.weatherService.getWeatherDatas('Sidney')
+    this.getWeatherDatas(this.cityName);
+  }
+
+  getWeatherDatas(cityName: string): void {
+    this.weatherService.getWeatherDatas(cityName)
       .subscribe({
         next: (response) => {
           response && (this.weatherDatas = response);
           console.log(this.weatherDatas);
         }
       })
+  }
+
+  onSubmit(): void {
+    this.getWeatherDatas(this.cityName);
+    this.cityName = '';
   }
 }
