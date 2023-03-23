abc35d2274cde84731c18bac3949d84af03fe8f9
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2023-03-22 23:03:27 -0300

    Remove lib primeNg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,21 @@
-import { NgModule, OnInit } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from "@angular/common/http";
 import { FormsModule } from '@angular/forms';
 
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { PrimeNGConfig } from 'primeng/api';
-import { CardModule } from 'primeng/card';
-import { ButtonModule } from 'primeng/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WeatherHomeComponent } from './modules/weather/components/weather-home/weather-home.component';
+import { WeatherCardComponent } from './modules/weather/components/weather-card/weather-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WeatherHomeComponent
+    WeatherHomeComponent,
+    WeatherCardComponent
   ],
   imports: [
     BrowserModule,
@@ -26,19 +25,10 @@ import { WeatherHomeComponent } from './modules/weather/components/weather-home/
     FormsModule,
     // FontAwesome
     FontAwesomeModule,
-    // PrimeNg
-    CardModule,
-    ButtonModule,
-    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule implements OnInit {
+export class AppModule {
 
-  constructor(private primeNgConfig: PrimeNGConfig) { }
-
-  ngOnInit(): void {
-    this.primeNgConfig.ripple = true;
-  }
 }
