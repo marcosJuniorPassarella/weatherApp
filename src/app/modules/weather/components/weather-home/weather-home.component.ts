62acd54d455ed92eda2f494ab84183774ee68c90
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2023-04-06 22:02:30 -0300

    Implement prevent memory leak strategy

--- src/app/modules/weather/components/weather-home/weather-home.component.ts
+++ src/app/modules/weather/components/weather-home/weather-home.component.ts
@@ -1,37 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 import { WeatherService } from '../../services/weather.service';
 import { WeatherDatas } from 'src/app/models/weather.interface';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-weather-home',
   templateUrl: './weather-home.component.html',
-  styleUrls: []
+  styleUrls: [],
 })
-export class WeatherHomeComponent implements OnInit {
+export class WeatherHomeComponent implements OnInit, OnDestroy {
+  private readonly destroy$: Subject<void> = new Subject();
   weatherDatas!: WeatherDatas;
   initialCityName: string = 'São Paulo';
   searchIcon = faMagnifyingGlass;
 
-  constructor(private weatherService: WeatherService) { }
+  constructor(private weatherService: WeatherService) {}
 
   ngOnInit(): void {
     this.getWeatherDatas(this.initialCityName);
   }
 
   getWeatherDatas(cityName: string): void {
-    this.weatherService.getWeatherDatas(cityName)
+    this.weatherService
+      .getWeatherDatas(cityName)
+      .pipe(takeUntil(this.destroy$))
       .subscribe({
         next: (response) => {
           response && (this.weatherDatas = response);
-          console.log(this.weatherDatas);
-        }
-      })
+        },
+      });
   }
 
   onSubmit(): void {
     this.getWeatherDatas(this.initialCityName);
     this.initialCityName = '';
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
